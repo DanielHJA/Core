baacb581339578c201eba4ac99fc6afc2522d4f7
Daniel <daniel.hjartstrom@gmail.com>
2019-11-22 08:56:53 +0100

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Daniel Hjärtström on 2019-11-22.
+//  Copyright © 2019 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
